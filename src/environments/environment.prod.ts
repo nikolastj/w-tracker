4e1d3ec84b1bcb11457a58652a197855a9db5343
nikolastj <me.nikola@gmail.com>
2025-09-01 22:02:22 +0200

    update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.example.com', // example production API
+  apiUrl: 'https://workout-tracker-light-be-production.up.railway.app/', // example production API
   // Add other production environment variables here
 };
