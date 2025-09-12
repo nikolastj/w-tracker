9b908e489d914e93d9e936cb248308b67710e14d
nikolastj <me.nikola@gmail.com>
2025-09-12 12:35:00 +0200

    remove old comp

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,6 +1,5 @@
 export * from './app-header.component';
 export * from './app-layout.component';
-export * from './deactivate-protect.component';
 export * from './loader.component';
 export * from './public-header.component';
 export * from './public-layout.component';
